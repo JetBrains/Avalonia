0a1b6aee6bd694a1e1203674a2af693aa3cac967
stepan_govorko <stepan.govorko@jetbrains.com>
2020-10-30 17:28:08 +0300

    provide an ability to override Dispatcher (H.K.: cherrypick changes from b9a8660d/ac38ebc3/dca723df/80abd52e, d33c46ff/beb2dc6a/17eaa5f9/89accaf6, 366cbe92/5b0eb03a/438cb2fb/2178d910, 3cbbc851/b11e387d/f7dc5eee/45fef94f, a7bae810/7726f7fd/83285b3e/2d25731d, df3be9d5, except moving avalonia code to another file (DispatcherImpl) to avoid merge issues)

--- src/Avalonia.Base/Threading/Dispatcher.cs
+++ src/Avalonia.Base/Threading/Dispatcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Platform;
+using JetBrains.Annotations;
 
 namespace Avalonia.Threading
 {
@@ -12,15 +13,15 @@ namespace Avalonia.Threading
     /// In Avalonia, there is usually only a single <see cref="Dispatcher"/> in the application -
     /// the one for the UI thread, retrieved via the <see cref="UIThread"/> property.
     /// </remarks>
-    public class Dispatcher : IDispatcher
+    public class DispatcherImpl : IDispatcherImpl
     {
         private readonly JobRunner _jobRunner;
         private IPlatformThreadingInterface? _platform;
 
-        public static Dispatcher UIThread { get; } =
-            new Dispatcher(AvaloniaLocator.Current.GetService<IPlatformThreadingInterface>());
+        public static DispatcherImpl UIThread { get; } =
+            new DispatcherImpl(AvaloniaLocator.Current.GetService<IPlatformThreadingInterface>());
 
-        public Dispatcher(IPlatformThreadingInterface? platform)
+        public DispatcherImpl(IPlatformThreadingInterface? platform)
         {
             _platform = platform;
             _jobRunner = new JobRunner(platform);
@@ -145,5 +146,128 @@ namespace Avalonia.Threading
                 _platform.Signaled += _jobRunner.RunJobs;
             }
         }
+
+        void IDispatcherImpl.EnsurePriority(DispatcherPriority currentPriority) => EnsurePriority(currentPriority);
+
+        void IDispatcherImpl.UpdateServices() => UpdateServices();
+    }
+    
+    /// <summary>
+    /// Provides services for managing work items on a thread.
+    /// </summary>
+    /// <remarks>
+    /// In Avalonia, there is usually only a single <see cref="Dispatcher"/> in the application -
+    /// the one for the UI thread, retrieved via the <see cref="UIThread"/> property.
+    /// </remarks>
+    public class Dispatcher : IDispatcher
+    {
+        private IDispatcherImpl _dispatcherImpl;
+
+        public static Dispatcher UIThread { get; } =
+            new Dispatcher(AvaloniaLocator.Current.GetService<IDispatcherImpl>());
+
+        private Dispatcher([NotNull] IDispatcherImpl dispatcherImpl)
+        {
+            _dispatcherImpl = dispatcherImpl ?? throw new ArgumentNullException(nameof(dispatcherImpl));
+        }
+
+        public Dispatcher(IPlatformThreadingInterface platform)
+        {
+            _dispatcherImpl = new DispatcherImpl(platform);
+        }
+
+        public bool CheckAccess()
+        {
+            return _dispatcherImpl.CheckAccess();
+        }
+
+        public void VerifyAccess()
+        {
+            _dispatcherImpl.VerifyAccess();
+        }
+
+        public void Post(Action action, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            _dispatcherImpl.Post(action, priority);
+        }
+
+        public Task InvokeAsync(Action action, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            return _dispatcherImpl.InvokeAsync(action, priority);
+        }
+
+        public Task<TResult> InvokeAsync<TResult>(Func<TResult> function, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            return _dispatcherImpl.InvokeAsync(function, priority);
+        }
+
+        public Task InvokeAsync(Func<Task> function, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            return _dispatcherImpl.InvokeAsync(function, priority);
+        }
+
+        public Task<TResult> InvokeAsync<TResult>(Func<Task<TResult>> function, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            return _dispatcherImpl.InvokeAsync(function, priority);
+        }
+
+        public void MainLoop(CancellationToken cancellationToken)
+        {
+            _dispatcherImpl.MainLoop(cancellationToken);
+        }
+
+        public void RunJobs()
+        {
+            _dispatcherImpl.RunJobs();
+        }
+
+        public void RunJobs(DispatcherPriority minimumPriority)
+        {
+            _dispatcherImpl.RunJobs(minimumPriority);
+        }
+        
+        public void EnsurePriority(DispatcherPriority currentPriority)
+        {
+            _dispatcherImpl.EnsurePriority(currentPriority);
+        }
+
+        public void UpdateServices()
+        {
+            _dispatcherImpl.UpdateServices();
+        }
+    }
+    
+    public interface IDispatcherImpl : IDispatcher
+    {
+        /// <summary>
+        /// Runs the dispatcher's main loop.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// A cancellation token used to exit the main loop.
+        /// </param>
+        void MainLoop(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Runs continuations pushed on the loop.
+        /// </summary>
+        void RunJobs();
+
+        /// <summary>
+        /// Use this method to ensure that more prioritized tasks are executed
+        /// </summary>
+        /// <param name="minimumPriority"></param>
+        void RunJobs(DispatcherPriority minimumPriority);
+
+        /// <summary>
+        /// This is needed for platform backends that don't have internal priority system (e. g. win32)
+        /// To ensure that there are no jobs with higher priority
+        /// </summary>
+        /// <param name="currentPriority"></param>
+        void EnsurePriority(DispatcherPriority currentPriority);
+
+        /// <summary>
+        /// Allows unit tests to change the platform threading interface.
+        /// </summary>
+        void UpdateServices();
     }
 }
