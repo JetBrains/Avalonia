7fd15f4042233fe2af302af9c695f87912f3d821
stepan_govorko <stepan.govorko@jetbrains.com>
2020-11-03 14:27:54 +0100

    fixed Api

--- src/Windows/Avalonia.Win32/Win32Platform.cs
+++ src/Windows/Avalonia.Win32/Win32Platform.cs
@@ -137,6 +137,7 @@ namespace Avalonia.Win32
                 .Bind<IKeyboardDevice>().ToConstant(WindowsKeyboardDevice.Instance)
                 .Bind<IPlatformSettings>().ToConstant(s_instance)
                 .Bind<IPlatformThreadingInterface>().ToConstant(s_instance)
+                .Bind<IDispatcherImpl>().ToConstant(DispatcherImpl.UIThread)
                 .Bind<IRenderLoop>().ToConstant(new RenderLoop())
                 .Bind<IRenderTimer>().ToConstant(new DefaultRenderTimer(60))
                 .Bind<ISystemDialogImpl>().ToSingleton<SystemDialogImpl>()
