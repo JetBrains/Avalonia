28d4402c3dc35a39a86cca252afcec48aa17c098
stepan_govorko <stepan.govorko@jetbrains.com>
2020-11-05 12:54:47 +0100

    fixed WpfAvaloniaHost Infinite measure

--- src/Windows/Avalonia.Win32.Interop/Wpf/WpfTopLevelImpl.cs
+++ src/Windows/Avalonia.Win32.Interop/Wpf/WpfTopLevelImpl.cs
@@ -51,6 +51,11 @@ namespace Avalonia.Win32.Interop.Wpf
                 Renderer?.Resized(clientSize);
             }
 
+            protected override Size MeasureOverride(Size availableSize)
+            {
+                return base.MeasureOverride(ClientSize.Constrain(availableSize));
+            }
+
             public Size AllocatedSize => ClientSize;
         }
 
