06adf327cf17a5284cbcf39b9c6f21760d8ed212
Herman Kirshin <herman.kirshin@jetbrains.com>
2023-05-26 17:01:36 +0300

    Stop tooltip timer on pointer pressed to avoid hanging tooltips in some cases (i.e. when modal window is opened)

--- src/Avalonia.Controls/ToolTipService.cs
+++ src/Avalonia.Controls/ToolTipService.cs
@@ -2,6 +2,7 @@ using System;
 using Avalonia.Input;
 using Avalonia.Input.Raw;
 using Avalonia.Reactive;
+using Avalonia.Interactivity;
 using Avalonia.Threading;
 
 namespace Avalonia.Controls
@@ -128,6 +129,17 @@ namespace Avalonia.Controls
         {
             var control = (Control)e.Sender;
 
+            if (e.OldValue != null)
+            {
+                control.RemoveHandler(InputElement.PointerPressedEvent, ControlPointerPressed);
+            }
+
+            if (e.NewValue != null)
+            {
+                control.AddHandler(InputElement.PointerPressedEvent, ControlPointerPressed,
+                    RoutingStrategies.Bubble | RoutingStrategies.Tunnel | RoutingStrategies.Direct, true);
+            }
+
             if (ToolTip.GetIsOpen(control) && e.NewValue != e.OldValue && !(e.NewValue is ToolTip))
             {
                 if (e.NewValue is null)
@@ -180,6 +192,11 @@ namespace Avalonia.Controls
                     StartShowTimer(showDelay, newValue);
                 }
             }
+        }
+
+        private void ControlPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            StopTimer();
         }
 
         private void ToolTipClosed(object? sender, EventArgs e)
