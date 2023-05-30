92c613d3799c9ea78d540c1704cfc29c80b62399
Herman Kirshin <herman.kirshin@jetbrains.com>
2023-05-30 13:28:45 +0300

    fixed spamming gtk threads

--- src/Avalonia.X11/NativeDialogs/Gtk.cs
+++ src/Avalonia.X11/NativeDialogs/Gtk.cs
@@ -260,7 +260,6 @@ namespace Avalonia.X11.NativeDialogs
 
         public static Task<bool> StartGtk()
         {
-            return StartGtkCore();
             lock (s_startGtkLock)
                 return s_startGtkTask ??= StartGtkCore();
         }
