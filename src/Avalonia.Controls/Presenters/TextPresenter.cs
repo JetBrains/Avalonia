72e12f93667f209cd7b1bfc2211529c2efde6c86
Herman Kirshin <herman.kirshin@jetbrains.com>
2023-09-07 19:27:28 +0300

    Workaround for caret position after last character

--- src/Avalonia.Controls/Presenters/TextPresenter.cs
+++ src/Avalonia.Controls/Presenters/TextPresenter.cs
@@ -447,15 +447,6 @@ namespace Avalonia.Controls.Presenters
             var y = Math.Floor(_caretBounds.Y) + 0.5;
             var b = Math.Ceiling(_caretBounds.Bottom) - 0.5;
 
-            var caretIndex = _lastCharacterHit.FirstCharacterIndex + _lastCharacterHit.TrailingLength;
-            var lineIndex = TextLayout.GetLineIndexFromCharacterIndex(caretIndex, _lastCharacterHit.TrailingLength > 0);
-            var textLine = TextLayout.TextLines[lineIndex];
-
-            if (_caretBounds.X > 0 && _caretBounds.X >= textLine.WidthIncludingTrailingWhitespace)
-            {
-                x -= 1;
-            }
-
             return (new Point(x, y), new Point(x, b));
         }
 
@@ -617,6 +608,15 @@ namespace Avalonia.Controls.Presenters
             InvalidateArrange();
 
             var textWidth = TextLayout.OverhangLeading + TextLayout.WidthIncludingTrailingWhitespace + TextLayout.OverhangTrailing;
+
+            var caretIndex = _lastCharacterHit.FirstCharacterIndex + _lastCharacterHit.TrailingLength;
+            var lineIndex = TextLayout.GetLineIndexFromCharacterIndex(caretIndex, _lastCharacterHit.TrailingLength > 0);
+            var textLine = TextLayout.TextLines[lineIndex];
+
+            if (_caretBounds.X > 0 && _caretBounds.X >= textLine.WidthIncludingTrailingWhitespace)
+            {
+                textWidth += 1;
+            }
 
             return new Size(textWidth, TextLayout.Height);
         }
