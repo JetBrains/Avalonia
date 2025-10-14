2f14134ee058703d2f640d0dfd81c214a63e58b1
Herman Kirshin <herman.kirshin@jetbrains.com>
2025-10-14 11:48:55 +0300

    Added Ctrl+Click support for opening context menu on mac

--- src/Avalonia.X11/X11Platform.cs
+++ src/Avalonia.X11/X11Platform.cs
@@ -81,6 +81,7 @@ namespace Avalonia.X11
                     ? new GlibDispatcherImpl(this)
                     : new X11PlatformThreading(this))
                 .Bind<IRenderTimer>().ToConstant(timer)
+                .Bind<PlatformPointerConfiguration>().ToSingleton<PlatformPointerConfiguration>()
                 .Bind<PlatformHotkeyConfiguration>().ToConstant(new PlatformHotkeyConfiguration(KeyModifiers.Control))
                 .Bind<KeyGestureFormatInfo>().ToConstant(new KeyGestureFormatInfo(new Dictionary<Key, string>() { }, meta: "Super"))
                 .Bind<IKeyboardDevice>().ToFunc(() => KeyboardDevice)
